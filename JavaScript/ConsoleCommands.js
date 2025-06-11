9df4c64df05cf181bfd7b036563b1d0b9f76aa24
thakurabhishekgit <abhishek.thakur.mainacc@gmail.com>
2025-06-11 09:19:58 +0530

    consoleCommands

---
+++ JavaScript/ConsoleCommands.js
@@ -0,0 +1,19 @@
+alert("hello world");
+// This is a simple JavaScript file that displays an alert with the message "hello world".
+// It can be used to test if JavaScript is running correctly in the browser.
+// You can include this file in your HTML to see the alert when the page loads.
+
+confirm("Are you ready to proceed?");
+// This is a simple JavaScript file that displays a confirmation dialog with the message "Are you ready to proceed?".
+// It can be used to test if JavaScript is running correctly in the browser.
+// You can include this file in your HTML to see the confirmation dialog when the page loads.
+
+prompt("What is your name?");
+// This is a simple JavaScript file that displays a prompt dialog with the message "What is your name?".
+// It can be used to test if JavaScript is running correctly in the browser.
+// You can include this file in your HTML to see the prompt dialog when the page loads.
+
+console.log("hello world");
+// This is a simple JavaScript file that logs the message "hello world" to the console.
+// It can be used to test if JavaScript is running correctly in the browser.
+// You can include this file in your HTML to see the log message when the page loads.
