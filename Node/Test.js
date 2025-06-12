0af72566743406f29c8d7257cd92adc007cc5e3e
thakurabhishekgit <abhishek.thakur.mainacc@gmail.com>
2025-06-12 13:59:31 +0530

    Test

--- Node/Test.js
+++ Node/Test.js
@@ -1,12 +1,46 @@
-class NameValid {
-    constructor(FirstName , LastName) {
-        this.FirstName = FirstName;
-        this.LastName = LastName;
+// class NameValid {
+//     constructor(FirstName , LastName) {
+//         this.FirstName = FirstName;
+//         this.LastName = LastName;
+//     }
+//     fullName() {
+//         return `${this.FirstName} ${this.LastName}`;
+//     }
+// }
+
+// const person = new NameValid("Thakur", "Abhishek");
+// console.log(person.fullName());
+
+
+// //user input 
+// import readline from 'readline';
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// rl.question('Enter your name: ', (name) => {
+//   console.log(`Hello, ${name}!`);
+//   rl.close();
+// });
+
+
+class Rectangle {
+    constructor(length, width) {
+        this.length = length;
+        this.width = width;
     }
-    fullName() {
-        return `${this.FirstName} ${this.LastName}`;
+
+    area() {
+        return this.length * this.width;
+    }
+
+    perimeter() {
+        return 2 * (this.length + this.width);
     }
 }
 
-const person = new NameValid("Thakur", "Abhishek");
-console.log(person.fullName());
+const rectangle = new Rectangle(5, 3);
+console.log(`Area: ${rectangle.area()}`);
+console.log(`Perimeter: ${rectangle.perimeter()}`);
