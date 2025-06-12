0ad0b1ecda646091ae1b599c17ef9963e8d62cca
thakurabhishekgit <abhishek.thakur.mainacc@gmail.com>
2025-06-12 09:56:57 +0530

    async / await , promises  , call back functions

---
+++ Node/Basics.js
@@ -0,0 +1,48 @@
+// function fetchData(callback) {
+//     setTimeout(() => {
+//       console.log("Data fetched");
+//       callback(); // Executes after data fetching
+//     }, 2000);
+//   }
+  
+//   function processData() {
+//     console.log("Processing data...");
+//   }
+  
+//   fetchData(processData);
+  
+
+
+
+ //promises 
+ 
+//  const fetchData = () => {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         const success = true;
+//         success ? resolve("Data received") : reject("Error fetching");
+//       }, 2000);
+//     });
+//   };
+  
+//   fetchData()
+//     .then((data) => console.log(data))
+//     .catch((error) => console.log(error));
+
+
+
+// Async/Await example
+const fetchData = () => {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve("Data loaded"), 2000);
+    });
+  };
+  
+  const load = async () => {
+    const result = await fetchData();
+    console.log(result);
+  };
+  
+  load();
+  
+  
