5941c84a62ff13c855f2a786cb854435046c2fd1
thakurabhishekgit <abhishek.thakur.mainacc@gmail.com>
2025-06-12 10:47:04 +0530

    type module

--- Node/Anurag.js
+++ Node/Anurag.js
@@ -1,16 +1,13 @@
-let student = ["a", "b", "c", "d", "e"];
-let branches = ["branch1", "branch2", "branch3"];
+export let student = ["a", "b", "c", "d", "e"];
+export let branches = ["branch1", "branch2", "branch3"];
 
-let studentDetails = {
+export let studentDetails = {
   name: "Anurag",
   age: 20,
   city: "Delhi",
   branch: "branch1",
 };
 
-// Exporting the student array and branches array
-module.exports = {
-  student,
-  branches,
-  studentDetails,
+export let a = () => {
+  console.log("This is a function in Anurag.js");
 };
--- Node/MainDemo.js
+++ Node/MainDemo.js
@@ -1,4 +1,6 @@
-const data = require("./Anurag");
-console.log("Student Array:", data.student);
-console.log("Branches Array:", data.branches);
-console.log("Student Details:", data.studentDetails);
+import { student, branches, studentDetails, a } from "./Anurag.js";
+console.log("Student Array:", student);
+console.log("Branches Array:", branches);
+console.log("Student Details Object:", studentDetails);
+console.log("Function from Anurag.js:");
+a();
