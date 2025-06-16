22e0bae5e6a4de9a571e67882881f9eaf0f829ff
thakurabhishekgit <abhishek.thakur.mainacc@gmail.com>
2025-06-16 11:24:31 +0530

    fs module

--- Node/NodeExp/index.js
+++ Node/NodeExp/index.js
@@ -1,2 +1,6 @@
-import os from "os";
-console.log(os.platform());
+// import os from "os";
+// console.log(os.platform());
+// console.log(os.cpus().length);
+
+// import fs from 'fs';
+// fs.writeFileSync('hello.txt', 'Hello World!');
