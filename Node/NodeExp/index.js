d4be4984ac883f79b081bdeddfe10b6478f0f89a
thakurabhishekgit <abhishek.thakur.mainacc@gmail.com>
2025-06-16 12:06:35 +0530

    date

--- Node/NodeExp/index.js
+++ Node/NodeExp/index.js
@@ -2,5 +2,23 @@
 // console.log(os.platform());
 // console.log(os.cpus().length);
 
-// import fs from 'fs';
-// fs.writeFileSync('hello.txt', 'Hello World!');
+// import fs from "fs";
+// fs.readFile("index.js", "utf-8", (err, data) => {
+//     if (err) {
+//         console.error(err);
+//     } else {
+//         console.log(data);
+//     }
+// });
+
+const date = new Date();
+console.log(date);
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDate());
+console.log(date.getDay());
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
+console.log(date.getMilliseconds());
+
