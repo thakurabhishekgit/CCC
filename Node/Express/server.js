a5bf879bc64256f60be41f0e5a25d4044180cba3
thakurabhishekgit <abhishek.thakur.mainacc@gmail.com>
2025-06-17 14:25:08 +0530

    express jsonn

--- Node/Express/server.js
+++ Node/Express/server.js
@@ -4,8 +4,29 @@ const app = express()
 const port = 8000
 
 const myPath = path.join(import.meta.dirname , "public" )
-
+app.use(express.json());
 app.use(express.static(myPath))
+app.use(express.urlencoded({extended : true}))
+let student = [
+    {
+        name : "Anurag"
+    }
+
+]
+
+app.post("/addData" , (req , res) => {
+    student.push(req.body)
+    res.json({
+        status : "success",
+        data : req.body
+    })
+})
+
+
+app.get("/addData" , (req , res) => {
+    res.json(JSON.stringify(student))
+})
+
 
 app.listen(port, () => {
     console.log(`server is running on http://localhost:${port}`)
